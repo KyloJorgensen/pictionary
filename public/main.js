c4c0fe69f7a820a0dc67ae50ffb9e39f445805db
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-08-22 11:48:59 -0600

    added features the pic game. word generation, chooses a new artist when the first one leaves, when a guesser is right hey become the next artist.

---
+++ public/main.js
@@ -0,0 +1,84 @@
+var WORDS = ["word", "letter", "number", "person", "pen", "class", "people", "sound", "water", "side", "place", "man", "men", "woman", "women", "boy", "girl", "year", "day", "week", "month", "name", "sentence", "line", "air", "land", "home", "hand", "house", "picture", "animal", "mother", "father", "brother", "sister", "world", "head", "page", "country", "question", "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree", "farm", "story", "sea", "night", "day", "life", "north", "south", "east", "west", "child", "children", "example", "paper", "music", "river", "car", "foot", "feet", "book", "science", "room", "friend", "idea", "fish", "mountain", "horse", "watch", "color", "face", "wood", "list", "bird", "body", "dog", "family", "song", "door", "product", "wind", "ship", "area", "rock", "order", "fire", "problem", "piece", "top", "bottom", "king", "space"];
+
+var pictionary = function(socket) {
+    var drawer = false;
+
+    socket.on('artist', function(data) {
+        console.log(data);
+        drawer = data.artist;
+        if (drawer) {
+            $('#guess').hide();
+            $('#artist').show();
+            $('#artist-word').html(data.word);
+        } else {
+            $('#guess').show();
+            $('#artist').hide();
+        }
+    });
+
+    socket.on('connections', function() {
+        socket.emit('connections', drawer);
+    });
+
+    socket.on('newgame' , function(message) {
+        console.log(message);
+        socket.emit('newgame', '');
+    });
+
+    var canvas, context;
+
+    var draw = function(position) {
+        context.beginPath();
+        context.arc(position.x, position.y,
+                         6, 0, 2 * Math.PI);
+        context.fill();
+    };
+
+    canvas = $('canvas');
+    context = canvas[0].getContext('2d');
+    canvas[0].width = canvas[0].offsetWidth;
+    canvas[0].height = canvas[0].offsetHeight;
+    var drawing = false;
+    canvas.on('mousemove', function(event) {
+        var offset = canvas.offset();
+        var position = {x: event.pageX - offset.left,
+                        y: event.pageY - offset.top};
+        if (drawing && drawer) {
+            draw(position);
+            socket.emit('draw', position);
+        }
+    }).on('mousedown', function() {
+        drawing = true;
+    }).on('mouseup', function() {
+        drawing = false;
+    });
+
+    socket.on('draw', draw);
+
+    var guessBox;
+
+    var addGuess = function(guess) {
+        lastGuess.html(guess);
+    };
+
+    var onKeyDown = function(event) {
+        if (event.keyCode != 13) { // Enter
+            return;
+        }
+        addGuess(guessBox.val());
+        socket.emit('guess', guessBox.val());
+        guessBox.val('');
+    };
+
+    guessBox = $('#guess input');
+    lastGuess = $('#lastGuess');
+    guessBox.on('keydown', onKeyDown);
+
+    socket.on('guess', addGuess);
+
+};
+
+$(document).ready(function() {
+    var socket = io();
+    pictionary(socket);
+});
--- server/server.js
+++ server/server.js
@@ -1,13 +1,96 @@
 'use stirct';
 
 var http = require('http'),
+	path = require('path'),
 	express = require('express'),
 	socket_io = require('socket.io'),
 	app = express();
+
+var WORDS = ["word", "letter", "number", "person", "pen", "class", "people", "sound", "water", "side", "place", "man", "men", "woman", "women", "boy", "girl", "year", "day", "week", "month", "name", "sentence", "line", "air", "land", "home", "hand", "house", "picture", "animal", "mother", "father", "brother", "sister", "world", "head", "page", "country", "question", "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree", "farm", "story", "sea", "night", "day", "life", "north", "south", "east", "west", "child", "children", "example", "paper", "music", "river", "car", "foot", "feet", "book", "science", "room", "friend", "idea", "fish", "mountain", "horse", "watch", "color", "face", "wood", "list", "bird", "body", "dog", "family", "song", "door", "product", "wind", "ship", "area", "rock", "order", "fire", "problem", "piece", "top", "bottom", "king", "space"];
+
 
 app.use(express.static('public'));
 
 var server = http.Server(app);
 var io = socket_io(server);
+var artist = true;
+var randomWord = 'default';
+io.on('connection', function (socket) {
+
+    console.log('Client connected');
+
+
+    socket.emit('artist', function() {
+    	var data = {};
+    	data.artist = artist;
+    	if (data.artist) {
+    		generateRandomWord();
+    		data.word = randomWord;
+    	} 
+    	artist = false;
+    	console.log(data);
+    	return data;
+    }());
+
+    function generateRandomWord() {
+    	randomWord = WORDS[Math.floor(Math.random() * WORDS.length)];
+    }
+
+    socket.on('newgame', function() {
+	    socket.emit('artist', function() {
+	    	var data = {};
+	    	data.artist = artist;
+	    	if (data.artist) {
+	    		generateRandomWord();
+	    		data.word = randomWord;
+	    	} 
+	    	artist = false;
+	    	console.log(data);
+	    	return data;
+	    }());
+    });
+
+    socket.on('disconnect', function() {
+        console.log('A user has disconnected');
+        socket.broadcast.emit('connections', '');
+    });
+
+    socket.on('connections', function(drawer) {
+		var newArtist = true,
+			artistConnected = false;  
+
+    	if (newArtist) {
+    		setTimeout(function() {
+    			if (!artistConnected) {
+    				artist = true;
+	    			io.sockets.emit('newgame', 'Artist left');
+	    		}
+    		}, 1000);
+    		newArtist = false;
+    	}
+
+    	if (drawer) {
+    		artistConnected = true;
+    	}
+
+    });
+
+    socket.on('draw', function(position) {
+        socket.broadcast.emit('draw', position);
+    });
+
+    socket.on('guess', function(guess) {
+    	socket.broadcast.emit('guess', guess);
+    	if (guess == randomWord){
+    		socket.broadcast.emit('newgame', 'Word Was Guessed Correctly');
+    		generateRandomWord();
+    		socket.emit('artist', {artist: true, word: randomWord});
+    	}
+    });
+});
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '../public/index.html'))
+});
 
 server.listen(8080);
